7ea4e6526117a241d98d42062526c683b6964bbd
Ryan Hayden <ryanhayden@Ryans-MacBook-Pro.local>
2026-03-26 05:31:42 -0500

    Update for laravel 13

--- src/CongregationHubThemesServiceProvider.php
+++ src/CongregationHubThemesServiceProvider.php
@@ -8,7 +8,7 @@ class CongregationHubThemesServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        //
+        require_once __DIR__ . '/helpers.php';
     }
 
     public function boot(): void
