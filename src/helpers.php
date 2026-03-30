2435802dea439d9c84b5dde087088e7b68e577c5
Ryan Hayden <ryanhayden@Ryans-MacBook-Pro.local>
2026-03-30 05:56:17 -0500

    Fixed issue with markdown on notes

--- src/helpers.php
+++ src/helpers.php
@@ -14,9 +14,13 @@ if (! function_exists('markdown_to_html')) {
         }
 
         // If content is already HTML (legacy Trix/Tiptap content), return as-is
-        if (preg_match('/^<(p|div|h[1-6]|ul|ol|li|blockquote|strong|em|a|br|img)[\s>]/im', trim($text))) {
+        if (preg_match('/^<(p|div|h[1-6]|ul|ol|li|blockquote|strong|em|a|br|img)[\s>]/i', trim($text))) {
             return $text;
         }
+
+        // Unescape markdown syntax that tiptap-markdown escapes for round-tripping
+        $text = html_entity_decode($text, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+        $text = preg_replace('/\\\\([\\\\`*_{}\[\]()#+\-.!>~|])/', '$1', $text);
 
         // Encode spaces in markdown image/link URLs so CommonMark can parse them
         $text = preg_replace_callback('/(!?\[[^\]]*\])\(([^)]+)\)/', function ($matches) {
